bfc96a01e11351a9949bc182a9bfe49a76d1891c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-19 19:32:53 -0300

    feat: create and find bank account

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BankAccount } from './bank-accounts/entities/bank-account.entity';
+import { PixKeysModule } from './pix-keys/pix-keys.module';
 
 @Module({
-  imports: [BankAccountsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      database: 'nest',
+      username: 'postgres',
+      password: 'root',
+      synchronize: true,
+      entities: [BankAccount],
+    }),
+    BankAccountsModule,
+    PixKeysModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

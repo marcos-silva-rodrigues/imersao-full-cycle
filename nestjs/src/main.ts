a21fa9f61339515bb9cf3393100a8a32c215491c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-20 20:58:48 -0300

    feat: add filters

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PixKeyAlreadyExistsErrorFilter } from './pix-keys/filters/pix-key-already-exists.error';
+import { PixKeyGrpcUnknownErrorFilter } from './pix-keys/filters/pix-key-unknown.error';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(
+    new PixKeyAlreadyExistsErrorFilter(),
+    new PixKeyGrpcUnknownErrorFilter(),
+  );
   await app.listen(3000);
 }
 bootstrap();

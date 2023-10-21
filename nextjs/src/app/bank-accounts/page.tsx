c5eeb2464778753bd4de460c18589b4542db492a
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-21 16:07:24 -0300

    feat: home page

--- nextjs/src/app/bank-accounts/page.tsx
+++ nextjs/src/app/bank-accounts/page.tsx
@@ -1,7 +1,37 @@
-export default function HomePage() {
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { CardAction } from "@/components/CardAction";
+import { BankAccount } from "@/models";
+import { Typography } from '@mui/material'
+
+export async function getBankAccounts(): Promise<BankAccount[]> {
+  const response = await fetch('http://host.docker.internal:3000/bank-accounts', {
+    next: {
+      revalidate: 10
+    }
+  });
+  return response.json();
+}
+
+export async function HomePage() {
+  const bankAccounts = await getBankAccounts();
   return (
     <div>
-    <h1>Home Page</h1>
+      <Typography variant="h4">Contas bancárias</Typography>
+      <Grid2 container spacing={2} mt={1}>
+        {bankAccounts.map((bankAccount) => (
+          <Grid2 key={bankAccount.id} xs={12} sm={6} md={4}>
+            <CardAction>
+              <Typography variant="h5" component="div">
+                {bankAccount.owner_name}
+              </Typography>
+              <Typography component="span">
+                Conta: {bankAccount.account_number}
+              </Typography>
+            </CardAction>
+          </Grid2>
+        ))}
+      </Grid2>
     </div>
   )
 }
+export default HomePage;
